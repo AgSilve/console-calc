fbb8b6328a32dc260f410fe20d0e8a06db9365ea
AgSilve <86034982+AgSilve@users.noreply.github.com>
2024-04-11 23:46:12 +0530

    feat: Program performed addition of two integers. The first stage is completed. Minor modifications expected soon.

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -1,1 +1,32 @@
-﻿Console.WriteLine("Let's do simple arithmetic!");
+﻿Console.WriteLine("Let's do simple addition!");
+
+/*As of now, the program is set up to do arithmetic operation between two numbers entered by the user.
+*/
+
+Console.WriteLine("Enter the first number:");
+string? firstNumberInput = Console.ReadLine();
+
+Console.WriteLine("Enter the second number:");
+string? secondNumberInput = Console.ReadLine();
+
+int? firstNumber = null, secondNumber = null;
+
+if (firstNumberInput != null)
+{
+    firstNumber = int.Parse(firstNumberInput);
+}
+
+if (secondNumberInput != null)
+{
+    secondNumber = int.Parse(secondNumberInput);
+}
+
+if (firstNumber != null && secondNumber != null)
+{
+    int sum = firstNumber.Value + secondNumber.Value;
+    Console.WriteLine($"The sum of {firstNumber} and {secondNumber} is: {sum}");
+}
+else 
+{
+    Console.WriteLine("Invalid input. Please enter valid numbers.");
+}
