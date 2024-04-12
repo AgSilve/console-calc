fe648e2381571bce60545460e44545c0c6b9d3b5
AgSilve <86034982+AgSilve@users.noreply.github.com>
2024-04-12 10:38:21 +0530

    feat: Add subtraction functionality.
    - Implemented subtraction feature to allow users to subtract two numbers.
    - Updated user interface and error handling for subtraction operations.

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -1,9 +1,15 @@
-﻿Console.WriteLine("Let's do simple addition!");
+﻿Console.WriteLine("Let's do simple arithmetic!");
 
 /*As of now, the program is set up to do arithmetic operation between two numbers entered by the user.
 */
 
-Console.WriteLine("Enter the first number:");
+string[] operations = {"Addition", "Subtraction"} ;
+
+Console.WriteLine("What action would you like to take? Would you like to add or subtract?");
+string? userInputOperation = Console.ReadLine();
+if (userInputOperation == "add" || userInputOperation =="Add")
+{
+    Console.WriteLine("Enter the first number:");
 string? firstNumberInput = Console.ReadLine();
 
 Console.WriteLine("Enter the second number:");
@@ -12,11 +18,36 @@ string? secondNumberInput = Console.ReadLine();
 int firstNumber, secondNumber;
 
 if (int.TryParse(firstNumberInput, out firstNumber) && int.TryParse(secondNumberInput, out secondNumber))
-{
+    {
     int sum = firstNumber + secondNumber;
     Console.WriteLine($"The sum of {firstNumber} and {secondNumber} is: {sum}");
+    }
+else
+    {
+    Console.WriteLine("Invalid input!");
+    }
 }
-else 
+else if (userInputOperation == null)
 {
     Console.WriteLine("Invalid input!");
+}
+else if (userInputOperation == "subtract" || userInputOperation =="Subtract" || userInputOperation == "sub" || userInputOperation == "Sub")
+{
+     Console.WriteLine("Enter the first number:");
+string? firstNumberInput = Console.ReadLine();
+
+Console.WriteLine("Enter the second number:");
+string? secondNumberInput = Console.ReadLine();
+
+int firstNumber, secondNumber;
+
+if (int.TryParse(firstNumberInput, out firstNumber) && int.TryParse(secondNumberInput, out secondNumber))
+    {
+    int difference = firstNumber - secondNumber;
+    Console.WriteLine($"The difference of {firstNumber} and {secondNumber} is: {difference}");
+    }
+else
+    {
+    Console.WriteLine("Invalid input!");
+    }
 }
