e6e7594b434fb8c1294077b4c8c1944518083d8e
AgSilve <86034982+AgSilve@users.noreply.github.com>
2024-04-21 21:00:01 +0530

    Rewrote the whole program.

---
+++ SimpleCalculator/Calculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+// This program is designed to carry out simple arithmetic operations between two numbers.
+public class Calculator
+{ 
+    public double Add(double num1, double num2)
+    {
+        return num1 + num2 ;
+    }
+    public double Sub(double num1, double num2)
+    {
+        return num1 - num2 ;
+    }
+    public double Mult(double num1, double num2)
+    {
+        return num1 * num2 ;
+    }
+    public double Div(double num1, double num2)
+    {
+        return num1 / num2 ;
+    }
+}
